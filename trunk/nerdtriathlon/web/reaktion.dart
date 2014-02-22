434c1b5669e2f8f539ab61257e4a134bfaa562c4
daniela.zimmermann91@gmail.com <daniela.zimmermann91@gmail.com@14a5dfe1-8982-2f89-d045-51c5f459656b>
2014-02-22 15:11:52 +0000

    Funktionalität drin

--- trunk/nerdtriathlon/web/reaktion.dart
+++ trunk/nerdtriathlon/web/reaktion.dart
@@ -1,27 +1,65 @@
 import 'dart:html';
 import 'dart:async';
+import 'dart:math' show Random;
 
 Stopwatch mywatch = new Stopwatch();
 ButtonElement button;
 Timer timer;
+bool readyToCount = false;
+bool isCheating = false;
 
 void main() {
   button = querySelector('#play_button')
-  ..onMouseUp.listen(mouseUpEvent);
-  timer = new Timer(new Duration(seconds:5), go);
+  ..onMouseUp.listen(mouseUpEvent)
+  ..onMouseOut.listen(cheat)
+  ..onMouseDown.listen(mouseDownEvent);
 }
 
 void mouseUpEvent(MouseEvent event){
-  mywatch.stop();
-  var time = mywatch.elapsedMilliseconds;
-  button.text = time.toString();
-  button.disabled = true;
+  if(readyToCount) {
+    mywatch.stop();
+    button.disabled = true;
+    var time = mywatch.elapsedMilliseconds;
+    button.text = time.toString();
+    readyToCount = false;
+    isCheating = true;
+  } else if(isCheating) {
+    button.text = "Pressen!";
+    timer.cancel();
+    mywatch.stop();
+    mywatch.reset();
+    isCheating = false;
+    readyToCount = false;
+  
+  }
+}
+
+void mouseDownEvent(MouseEvent event){
+  isCheating = true;
+  readyToCount = false;
+  Random random = new Random();
+  int randomTime = random.nextInt(10);
+  timer = new Timer(new Duration(seconds:randomTime), go);
+  button.text = "Warten..";
 }
 
 void reset() {
   
 }
+
+void cheat(MouseEvent event) {
+  if(isCheating) {
+    button.text = "Pressen!";
+    timer.cancel();
+    mywatch.stop();
+    mywatch.reset();
+    isCheating = false;
+    readyToCount = false;
+  }
+}
+
 void go() {
+  readyToCount = true;
   mywatch.start();
   button.text = "LOSLASSEN!";
 }
