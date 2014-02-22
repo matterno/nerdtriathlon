8ee17edc9d9abfeac9e4e2b7ec36f4039002d5fc
Timo.Klostermann@gmail.com <Timo.Klostermann@gmail.com@14a5dfe1-8982-2f89-d045-51c5f459656b>
2014-02-22 15:43:33 +0000

    first server communication

--- trunk/nerdtriathlon/web/sendScore.dart
+++ trunk/nerdtriathlon/web/sendScore.dart
@@ -10,5 +10,20 @@ void main() {
 }
 
 void sendData(Event e) {
+  HttpRequest request = new HttpRequest(); // create a new XHR
+  // add an event handler that is called when the request finishes
+  request.onReadyStateChange.listen((_) 
+      {
+    if (request.readyState == HttpRequest.DONE &&
+        (request.status == 200 || request.status == 0)) {
+      // data saved OK.
+      print(request.responseText); // output the response from the server
+    }
+      }
+  );
   
+  var url = "http://127.0.0.1:8081/submit";
+  request.open("POST", url, async: false);
+  request.setRequestHeader("Content-Type", "application/json");
+  request.send('{"name" : "test", "gameReaction" : "200", "gameWord" : "300", "gameClick" : "400"}');
 }
